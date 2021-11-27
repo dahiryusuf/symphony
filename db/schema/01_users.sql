2537c25e6a0cb0437c9adc157f39c3314e563235
jtardioli <joshua.tardioli@gmail.com>
2021-11-27 12:52:55 -0500

    items table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,13 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  description text,
+  image VARCHAR(255) NOT NULL,
+  price INTERGER NOT NULL,
+  admin_id INTEGER
+  is_sold boolean
+  is_deleted
 );
