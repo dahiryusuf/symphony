10287027d61e63e90eb5a86d9fa39a070cc20300
jtardioli <joshua.tardioli@gmail.com>
2021-11-27 13:48:38 -0500

    add seed data

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -18,12 +18,12 @@ CREATE TABLE items (
   price INTERGER NOT NULL,
   admin_id INTEGER
   is_sold boolean
-  is_deleted
+  is_deleted boolean
 );
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY NOT NULL,
   chat_id INTEGER REFERENCES chats(id) ON DELETE CASCADE,
-  time_sent TIME,
+  time_sent NOW(),
   sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
 
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,67 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, is_admin, email, password) VALUES ('Alice', 'Wonderland',false,'alicewonder@gmail.com','12345');
+INSERT INTO users (first_name, last_name, is_admin, email, password) VALUES ('Bob', 'Smith',true,'Bobsmith@gmail.com','12345');
+INSERT INTO users (first_name, last_name, is_admin, email, password) VALUES ('Wonder', 'Wanda',false,'wonder@gmail.com','12345');
+
+
+INSERT INTO messages 
+VALUES (1, 1),
+(2, 2)
+(3, 3);
+
+INSERT INTO favourites
+VALUES (1, 1),
+(2, 2),
+(3, 3);
+
+INSERT INTO chats 
+VALUES (1, 1),
+(2, 2),
+(3, 3);
+
+
+
+INSTER INTO items VALUES (
+  1, 
+"Steinberg Piano", 
+'Black. Attractive cabinet with polished wood and grand styling
+88 key graded hammer action velocity sensitive
+128-note polyphony with sound effects engaged.
+Voice mode: Main, multi-layer for 4 voices
+122 selected voices',
+'https://m.media-amazon.com/images/I/61GrJRwTWVS._AC_SL1200_.jpg',
+5000,
+1,
+false,
+false
+)
+
+INSTER INTO items VALUES (
+  2, 
+"Fender Mustang", 
+'Black. Attractive cabinet with polished wood and grand styling
+88 key graded hammer action velocity sensitive
+128-note polyphony with sound effects engaged.
+Voice mode: Main, multi-layer for 4 voices
+122 selected voices',
+'https://www.sefiles.net/merchant/5582/images/zoom/IMG_1162.jpg',
+1100,
+1,
+false,
+false
+)
+
+INSTER INTO items VALUES (
+  3, 
+"Pearl Drums", 
+'Black. Attractive cabinet with polished wood and grand styling
+88 key graded hammer action velocity sensitive
+128-note polyphony with sound effects engaged.
+Voice mode: Main, multi-layer for 4 voices
+122 selected voices',
+'https://sc1.musik-produktiv.com/pic-010068549xxl/pearl-export-22-smokey-chrome-inkl-sabian-sbr-beckenset.jpg',
+2500,
+1,
+false,
+false
+)
