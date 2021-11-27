57475ad2bfb82ad3d435f1552a5f8c32eab084b8
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-27 17:58:14 +0000

    Add table

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,6 +1,14 @@
 -- Drop and recreate Users table (Example)
 
 DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  is_admin BOOLEAN NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
