9b0a68d2aeafe0d48d6e22fd58f28f0bb7583ba6
dahir yusuf <dahir.yusuf@hotmail.com>
2021-11-30 04:02:39 +0000

    add favourites page

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router  = express.Router();
 const databases = require("../public/scripts/database");
 
+
   router.get("/login", (req, res) => {
     res.render("loginpage");
   });
   router.post("/login", (req, res) => {
-    console.log(req.body.username)
     res.cookie('User', req.body.username)
     res.redirect("/")
   });
