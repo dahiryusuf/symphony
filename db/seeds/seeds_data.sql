2db676afd626c142158a44f2c4aab4b2123b53f9
jtardioli <joshua.tardioli@gmail.com>
2021-12-02 18:52:11 -0500

    fix seed data

--- db/seeds/seeds_data.sql
+++ db/seeds/seeds_data.sql
@@ -18,7 +18,7 @@ INSERT INTO items (name,
 128-note polyphony with sound effects engaged.
 Voice mode: Main, multi-layer for 4 voices
 122 selected voices',
-'https://m.media-amazon.com/images/I/61GrJRwTWVS._AC_SL1200_.jpg',
+'https://www.chuppspianos.com/wp-content/uploads/2018/09/Kurtzmann-Co-Louis-XV-Style-Art-Case-Grand-Piano-67711-Circassian-Walnut.jpg',
 5000,
 2,
 false,
@@ -33,14 +33,10 @@ INSERT INTO items (name,
   is_sold,
   is_deleted)
   VALUES (
-'Steinberg Piano',
-'Black. Attractive cabinet with polished wood and grand styling
-88 key graded hammer action velocity sensitive
-128-note polyphony with sound effects engaged.
-Voice mode: Main, multi-layer for 4 voices
-122 selected voices',
-'https://m.media-amazon.com/images/I/61GrJRwTWVS._AC_SL1200_.jpg',
-5000,
+'Mendini By Cecilio Violin',
+'Great Violin For Beginners: The beginner violin is an ideal stringed musical instrument for any student who has dreams of playing music. The set includes all the necessities to start learning how to play.',
+'https://viewthevibe.com/wp-content/uploads/2020/06/Violin.jpg',
+600,
 2,
 false,
 false
@@ -54,14 +50,14 @@ INSERT INTO items (name,
   is_sold,
   is_deleted)
   VALUES (
-'Steinberg Piano',
-'Black. Attractive cabinet with polished wood and grand styling
-88 key graded hammer action velocity sensitive
-128-note polyphony with sound effects engaged.
-Voice mode: Main, multi-layer for 4 voices
-122 selected voices',
-'https://m.media-amazon.com/images/I/61GrJRwTWVS._AC_SL1200_.jpg',
-5000,
+'Ibanez 4 String Bass Guitar',
+'Fast, slim Maple neck
+Compact, light-weight Body
+Dynamic P pickup
+Short Scale 28. 6. great condition. selling because I found a new bass guitar"
+',
+'https://makingmusicmag.com/wp-content/uploads/2018/10/bass-guitar-1841186_1280.jpg',
+1200,
 2,
 false,
 false
@@ -97,13 +93,10 @@ INSERT INTO items (name,
   is_deleted)
   VALUES (
 'Pearl Drums',
-'Black. Attractive cabinet with polished wood and grand styling
-88 key graded hammer action velocity sensitive
-128-note polyphony with sound effects engaged.
-Voice mode: Main, multi-layer for 4 voices
-122 selected voices',
-'https://sc1.musik-produktiv.com/pic-010068549xxl/pearl-export-22-smokey-chrome-inkl-sabian-sbr-beckenset.jpg',
-2500,
+'The Masterworks Custom Creative Process.
+Letting you explore literally hundreds of options in sculpting your own unique drum build and look, Masterworks Custom gives you the freedom to customize a sound that is completely your own. The creative process begins with crafting your ideal shell construction from our finest hand selected shell veneers.',
+'https://cdn.shopify.com/s/files/1/1422/9358/products/VK-VAN-KLEEF-DRUMS-7EMPEST-TEMPEST-CAST-ALUMINIUM-DANNY-CAREY-SET-TOOL-SET-UP-DRUM-KIT-BLACK-POWDER-COAT-DRUMAZON_01_1000x.jpg?v=1585600757',
+3120,
 1,
 false,
 false
